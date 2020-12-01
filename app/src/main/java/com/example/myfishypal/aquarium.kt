e16f568419c2ee494ef3aad7f388eab1c4123810
EmmJay99 <mariajulia@live.ca>
2020-11-30 19:05:03 -0500

    Fishtank Basics
    aaaa

---
+++ app/src/main/java/com/example/myfishypal/aquarium.kt
@@ -0,0 +1,6 @@
+package com.example.myfishypal
+
+class aquarium (val idforprod: String,val tankname: String, val tanksize: String,val tanklights: String, val tankfilter: String, val tanksubstrate: String){
+
+}
+
